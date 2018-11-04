8e30a4e90e6d924603bd60e2e958e0e024d028d5
teityan <tenko0316yuki1@gmail.com>
2018-11-05 00:08:32 +0900

    Correction

--- src/main/java/org/usfirst/frc/team6909/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team6909/robot/Robot.java
@@ -1,27 +1,46 @@
 package org.usfirst.frc.team6909.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Robot extends IterativeRobot {
 	private XboxController driver;
 	private PWMTalonSRX m_leftArm;
+	private PWMTalonSRX m_rightArm;
 
-	RObot(){
-		driver= new XboxController(0);
-		//ポートを教える
+	Robot() {
+		// 入力を受け取るオブジェクト
+		driver = new XboxController(0);
+		// ポートを教える
 		m_leftArm = new PWMTalonSRX(5);
 		m_rightArm = new PWMTalonSRX(6);
 	}
 
 	public void teleopPeriodic() {
 		// 右スティックのy軸の値
-		double value = driver.getY(Hand.kLeft);
-		if (value > 0.2 || value < -0.2) {
+		double left = driver.getY(Hand.kLeft);
+		double right = driver.getY(Hand.kRight);
+		if (left < 0.2 || left > -0.2 && right < 0.2 || right > -0.2) {
 			// モーターの速度を設定
-			m_leftArm.set(value);
-			m_rightArm.set(-value);
+			m_leftArm.set(0);
+			m_rightArm.set(0);
+		} else {
+			if (left > 0.2 || left < -0.2) {
+				// モーターの速度を設定
+				m_leftArm.set(100);
+			} else {
+				// モーターの速度を設定
+				m_leftArm.set(0);
+			}
+			if (right > 0.2 || right < -0.2) {
+				// モーターの速度を設定
+				m_rightArm.set(100);
+			} else {
+				// モーターの速度を設定
+				m_rightArm.set(0);
+			}
 		}
 	}
 }
